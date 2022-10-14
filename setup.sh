47572e3c709250f26955f31105668dc0a426f837
sven <sven@so36.net>
2022-10-14 12:58:38 +0200

    chore(repo) add bash colors

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Vars
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+#COL_YELLOW=$ESC_SEQ"33;01m"
+COL_RED=$ESC_SEQ"31;01m"
+#COL_GREEN=$ESC_SEQ"32;01m"
+
+
 # ------- SSH Key for GitHub -------
 echo "Creating an SSH key for you... 🔐"
 ssh-keygen -t rsa
